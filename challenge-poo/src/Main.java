d5302d4c27758792e94e7aae99510aa09aee616b
anderson amantino <andersonamantino@gmail.com>
2023-01-30 09:04:11 -0300

    Class curso e mentoria criadas

---
+++ challenge-poo/src/Main.java
@@ -0,0 +1,8 @@
+import dio_desafio_dominio.Curso;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Curso curso1 = new Curso();
+        System.out.println(curso1);
+    }
+}
